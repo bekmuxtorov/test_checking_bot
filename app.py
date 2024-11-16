b61328e6e87094f7bc8a1b8450b4028d87018442
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-11-16 16:00:45 +0500

    Added add test functionality

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from aiogram import executor
 
 from loader import dp, db
-import middlewares, filters, handlers
+import middlewares
+import filters
+import handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
@@ -10,6 +12,7 @@ async def on_startup(dispatcher):
     await db.create()
     # await db.drop_users()
     await db.create_table_users()
+    await db.create_table_tests()
 
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
